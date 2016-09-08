5d233d0e4ce2047c04273abbb0060903233202cb
Martin Pettersson <martin.pettersson@omegapoint.se>
2016-09-08 12:35:21 +0200

    Some tutorial stuff

--- js/script1.js
+++ js/script1.js
@@ -4,6 +4,8 @@ app1.controller('ctrl1', function($scope) {
 	$scope.first = 1;
 	$scope.second = 1;
 
+	$scope.todoList = ['köpa mat', 'gå hem', 'programmera'];
+
 	$scope.updateValue = function() {
 		$scope.calculation = $scope.first + ' + ' + $scope.second
 			+ ' = ' + (+$scope.first + +$scope.second);
